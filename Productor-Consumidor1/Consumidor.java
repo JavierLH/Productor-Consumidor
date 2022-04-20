bd5dc2453457a703396b2bb396f03fc23752b13d
JavierLH <91098390+JavierLH@users.noreply.github.com>
2022-04-20 10:39:31 -0500

    Productor consumidor
    Productor consumidor

---
+++ Productor-Consumidor1/Consumidor.java
@@ -0,0 +1,35 @@
+
+
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Consumidor extends Thread {
+
+    private Buffer buffer;
+    private Ventana CV; 
+
+    public Consumidor(Buffer b) {
+        this.buffer = b;
+    }
+    
+    public void imprimeC(){
+        //System.out.printf(" %d ", this.area.get(i));
+        System.out.println("Un cliente toma de la estantería la pizza numero: ");
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                char c = this.buffer.consumir();
+                imprimeC();
+                System.out.println(c);
+                //System.out.println("Un cliente toma la pizza numero " + c + " de la estanteria");
+
+                sleep((int) (Math.random() * 4000));
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Consumidor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
